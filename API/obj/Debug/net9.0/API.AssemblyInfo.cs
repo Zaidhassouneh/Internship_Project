329c01548e77548552758c56617d66a7553bf6e0
Zaid <zaidhassouneh256@gmail.com>
2025-07-13 18:47:38 +0300

    more packages

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e73093c450d4b94264250f212e91fe84da5b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b645a0554edf4251e05d66712a4ee0e7db8dcf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
